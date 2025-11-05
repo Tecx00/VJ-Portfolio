4bf5a99cf0eb6c06766f49655035738b5d21fd4b
Tecx00 <vjsaco003@gmail.com>
2025-11-05 23:09:06 +0800

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/VJ-Portfolio/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
